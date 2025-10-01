8e17f56f40a37fe469b805d18f57916fffb5a007
Atharvk04 <156418587+Atharvk04@users.noreply.github.com>
2025-10-01 18:00:29 +0530

    Add files via upload

---
+++ Classwork/Nested if-else/positive negative and zero.c
@@ -0,0 +1,20 @@
+// Find number is positive, negative or zero
+void main ()
+{
+	int num = -9;
+	if(num > 0)
+	{
+		printf("number is positive");
+	}
+	else
+	{
+		if(num ==0)
+		{
+			printf("Number is zero");
+		}
+		else
+		{
+			printf("Number is negative");
+		}
+	}
+}
