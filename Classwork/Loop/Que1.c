2885bef50b26060c9d2595874ebda61823dab258
Atharvk04 <156418587+Atharvk04@users.noreply.github.com>
2025-10-01 18:00:03 +0530

    Add files via upload

---
+++ Classwork/Loop/Que1.c
@@ -0,0 +1,13 @@
+// printf fbs 10 times
+
+
+void main () {
+	
+	int i =1;
+	
+	while (i<=10)
+	{
+		printf("\nfbs");
+		i++;
+	}
+}
---
+++ Classwork/Loop/Que3.c
@@ -0,0 +1,11 @@
+// print 50 to 100
+
+void main ()
+{
+	int i = 50;
+	while (i<=100)
+	{
+		printf("\n%d",i);
+		i++;
+	}
+}
