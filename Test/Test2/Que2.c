f30e018e1fe78a10879700e31737a7cf19823878
Atharvk04 <156418587+Atharvk04@users.noreply.github.com>
2025-10-04 14:47:50 +0530

    Add files via upload

---
+++ Test/Test2/Que2.c
@@ -0,0 +1,19 @@
+//WAP to check if number is positive, negative or neutral(0).
+
+void main()
+{
+	int num = 0;
+	
+	if(num >0 )
+	{
+		printf("Number is Positive");	
+	}	
+	else if (num <0)
+	{
+		printf("Number is Negative");
+	}
+	else
+	{
+		printf("Number is neutral");
+	}
+} 
