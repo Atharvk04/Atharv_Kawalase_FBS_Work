28a81e422eab8ef8ae98cace77065279df8f2f05
Atharvk04 <156418587+Atharvk04@users.noreply.github.com>
2025-10-08 10:20:26 +0530

    Delete Classwork directory

--- Classwork/Loop/for/Que1.c
+++
@@ -1,9 +0,0 @@
-void main ()
-{
-	int i = 1;
-	
-	for (i=1;i<=10;i++)
-	{
-		printf("\n%d",i);
-	}
-}
--- Classwork/Loop/for/Que2.c
+++
@@ -1,9 +0,0 @@
-void main ()
-{
-	int n=5,i=1;
-	
-	for (i=1;i<=10;i++)
-	{
-		printf("\n%d",n*i);
-	}
-}
