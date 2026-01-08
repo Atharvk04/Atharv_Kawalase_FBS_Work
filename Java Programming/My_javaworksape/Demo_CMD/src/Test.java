97d2dc5b25620bb3f7bc259e2cfd2a1272fc1fe9
Atharvk04 <156418587+Atharvk04@users.noreply.github.com>
2026-01-08 08:53:19 +0530

    Add files via upload

---
+++ Java Programming/My_javaworksape/Demo_CMD/src/Test.java
@@ -0,0 +1,10 @@
+
+class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Argument od 0th Index: " +args[0]);
+		System.out.println("Argument od 1th Index: " +args[1]);
+	}
+
+}
