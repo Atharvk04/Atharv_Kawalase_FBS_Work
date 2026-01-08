d2bbfe9cd89b01d71a1524d526a0f998c6bfd923
Atharvk04 <156418587+Atharvk04@users.noreply.github.com>
2026-01-08 08:42:02 +0530

    Add files via upload

---
+++ Java Programming/Classwork/15-Dec-2025(main Static)/Test.java
@@ -0,0 +1,8 @@
+class Test
+{
+	public static void main(String [] args)
+	{	
+		System.out.println("\n " + "Given argument for 0 index: " + args [0]);
+		System.out.println("Given argument for 1 index: " + args [1]);
+	}	
+}
